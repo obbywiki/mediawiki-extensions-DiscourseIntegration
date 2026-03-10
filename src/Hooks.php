3159ad53e7ea01411b0e526e098a3bb92adf12d0
Wolfite <100052562+wlft@users.noreply.github.com>
2026-03-10 17:17:03 +1000

    fix: imrpove talk button finding

--- src/Hooks.php
+++ src/Hooks.php
@@ -58,21 +58,34 @@ class Hooks  {
 			'class' => 'discourse-talk-link'
 		];
         
-        $skin->getOutput()->addModuleStyles( [ 'ext.DiscourseIntegration.styles' ] );
+		$skin->getOutput()->addModuleStyles( [ 'ext.DiscourseIntegration.styles' ] );
 
-        if ( isset( $links['associated-pages'] ) ) {
-            $links['associated-pages']['talk'] = $newLink;
-            if ( isset( $links['associated-pages']['discussion'] ) ) {
-                unset( $links['associated-pages']['discussion'] );
-            }
-        }
-
-		if ( isset( $links['namespaces']['talk'] ) ) {
-			$links['namespaces']['talk'] = $newLink;
-		}
 		
-		if ( isset( $links['namespaces']['discussion'] ) ) {
-			unset( $links['namespaces']['discussion'] );
+		$found = false;
+		foreach ( $links as $group => &$linksInGroup ) {
+			if ( !is_array( $linksInGroup ) ) {
+				continue;
+			}
+			foreach ( $linksInGroup as $key => &$link ) {
+				if (
+					$key === 'talk' ||
+					$key === 'discussion' ||
+					( isset( $link['rel'] ) && $link['rel'] === 'discussion' ) ||
+					( isset( $link['id'] ) && $link['id'] === 'ca-talk' ) ||
+					str_ends_with( $key, '_talk' )
+				) {
+					$link = $newLink;
+					$found = true;
+				}
+			}
+		}
+
+		if ( !$found ) {
+			if ( isset( $links['associated-pages'] ) ) {
+				$links['associated-pages']['talk'] = $newLink;
+			} else {
+				$links['namespaces']['talk'] = $newLink;
+			}
 		}
 	}
 }
